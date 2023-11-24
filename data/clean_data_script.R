efda4a31c2b430859e12121afa1e7332b33dbe22
Mitchell Wake <mwak0006@student.monash.edu>
2023-11-24 11:55:12 +0800

    remove Time.Code and OECD column

--- data/clean_data_script.R
+++ data/clean_data_script.R
@@ -3,12 +3,11 @@ df <- read.csv("data/dataset_world_bank.csv")
 
 df_filtered <- df %>%
   filter(Time > 1999 & Time < 2021 & Country.Name != "Greece") %>%
+  select(-Time.Code, -OECD) %>%
   rename(
     Year = Time,
-    Time.Code = Time.Code,
     Country_Name = Country.Name,
     Country_Code = Country.Code,
-    OECD = OECD,
     LifeExp_Female = Life.expectancy.at.birth..female..years.,
     LifeExp_Male = Life.expectancy.at.birth..male..years.,
     LifeExp_Total = Life.expectancy.at.birth..total..years.,
@@ -29,7 +28,9 @@ df_filtered <- df %>%
     Mortality_U5_Female = as.numeric(Mortality_U5_Female),
     Mortality_U5_Male = as.numeric(Mortality_U5_Male),
     HealthExp_percent_GDP = as.numeric(HealthExp_percent_GDP)
-  ) %>%
-  
-  write.csv("data/cleaned_data.csv")
+  )
+
+# Write the cleaned data to a CSV file
+write.csv(df_filtered, "data/cleaned_data.csv", row.names = FALSE)
+
 
